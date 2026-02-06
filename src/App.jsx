e2770ac6f4b56f7dcbbcfcccb1d43b467c1471fd
LeBASupreme <145618021+LeBASupreme@users.noreply.github.com>
2026-02-06 21:48:28 +0100

    Test video 2

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,7 @@ function App() {
 
   return (
     <>
-      <h1>Deployed React App</h1>
-      <h2>React is the best framework</h2>
+      <h2>Deployed React App</h2>
       <p>
         Hello world! This is a deployed React app. You can trigger a redeploy by pushing to the main branch of the connected GitHub repository. Check the server logs for details.
       </p>
